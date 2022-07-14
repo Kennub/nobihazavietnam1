34e028b51eec032eed9c46e0d4ea0bc8e69fdcda
Kennub <tuantretrau1234@gmail.com>
2022-07-14 18:20:58 +0700

    fixed some bugs

--- commands/mute.js
+++ commands/mute.js
@@ -20,10 +20,6 @@ module.exports.run = (client, message, args) => {
       return client.sendEmbed("#ff0000", "Moderation", "Ủa vậy bạn bảo mình mute ai :frog:", "https://i.imgur.com/Ld1y87B.jpeg", "");
       if (!time){
         if (member) {
-          const info = {
-            name: member,
-            muted: true
-          }
           member
             .roles.add('953282221169594398')
             .then((member) => client.sendEmbed("#66ff33", "Moderation", `${member} đã bị khâu mồm :smile:`, "https://i.imgur.com/Ld1y87B.jpeg", ""))
@@ -38,13 +34,11 @@ module.exports.run = (client, message, args) => {
         member
           .roles.add('953282221169594398')
           .then((member) => client.sendEmbed("#66ff33", "Moderation", `${member} đã bị khâu mồm trong ${ms(ms(time))} :smile:`, "https://i.imgur.com/Ld1y87B.jpeg", ""))
-        muted = true
         setTimeout(function(){
           member
             .roles.remove('953282221169594398')
             .then((member) => client.sendEmbed("#66ff33", "Moderation", `${member} đã được mở khóa mồm :smile:`, "https://i.imgur.com/Ld1y87B.jpeg", ""))
-            .catch((err) => client.sendEmbed("#ff0000", "Moderation", "Mình không đủ thẩm quyền để khóa mõm người này :(", "https://i.imgur.com/Ld1y87B.jpeg", ""));
-          muted = false
+            .catch((err) => client.sendEmbed("#ff0000", "Moderation", "Mình không đủ thẩm quyền để làm việc này", "https://i.imgur.com/Ld1y87B.jpeg", ""));
         }, ms(time))
       } else {
         client.sendEmbed("#ff0000", "Moderation", "Hình như người này không có trong server", "https://i.imgur.com/Ld1y87B.jpeg", "");
