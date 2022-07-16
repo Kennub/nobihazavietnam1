67a43c3cdccc155b522bd9fb643ad4d60d262d33
Kennub <tuantretrau1234@gmail.com>
2022-07-16 14:51:24 +0700

    .

--- bot.js
+++ bot.js
@@ -26,7 +26,6 @@ const files = fs.readdirSync("./commands");
 files.forEach(file => {
     if (!file.endsWith(".js")) return;
     const name = file.split(".")[0];
-    console.log(name)
     const cmd = require("./commands/" + name);
     if (cmd) client.commands.set(name, cmd);
     console.log("- Loaded command: " + name);
