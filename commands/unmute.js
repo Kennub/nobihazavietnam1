7073220e5622d6585bc0239e370edd94e4a6f74b
Kennub <tuantretrau1234@gmail.com>
2022-07-21 09:17:30 +0700

    updated few things

--- commands/unmute.js
+++ commands/unmute.js
@@ -21,7 +21,7 @@ module.exports.run = (client, message, args) => {
         member
           .roles.remove('953282221169594398')
           .then((member) => client.sendEmbed("#66ff33", "Moderation", `${member} đã được mở khóa mồm :moyai:`, "https://i.imgur.com/Ld1y87B.jpeg", ""))
-          .catch((err) => client.sendEmbed("#ff0000", "Moderation", "Em chịu anh ơi :(", "https://i.imgur.com/Ld1y87B.jpeg", ""));
+          .catch((err) => client.sendEmbed("#ff0000", "Moderation", "Mình không đủ quyền hạn để làm việc này", "https://i.imgur.com/Ld1y87B.jpeg", ""));
       } else {
         client.sendEmbed("#ff0000", "Moderation", `${member} hiện đang không bị khóa mõm`, "https://i.imgur.com/Ld1y87B.jpeg", "")
       }
