1c6f2ead05700f003e8426a77cce069bdd0410bc
Kennub <tuantretrau1234@gmail.com>
2022-07-22 09:02:33 +0700

    updated few things

--- commands/games.js
+++ commands/games.js
@@ -8,9 +8,9 @@ module.exports.config = {
 }
 
 module.exports.run = (client, message, args) => {
-    if (!args[0]) return client.sendEmbed('#ff0000', 'Link Game', 'Hãy thử `' + client.config.prefix + "games help`", 'https://i.imgur.com/Ld1y87B.jpeg', '')
+    if (!args[0]) return client.sendEmbed('#ff0000', 'Trợ Giúp', 'Hãy thử `' + client.config.prefix + "games help`", 'https://i.imgur.com/Ld1y87B.jpeg', '')
     if (args[0] == "help") {
-        var translated_games_text = "", indie_games_text = "", youtubers_games_text = "", i = 1;
+        var translated_games_text = "", indie_games_text = "", youtubers_games_text = "", coming_soon_games_text = "", i = 1;
         for (const key in client.config.games.translated_games) {
             translated_games_text += i + ". " + client.config.games.translated_games[key].name + ": `" + key + "`\n";
             i++;
@@ -25,20 +25,26 @@ module.exports.run = (client, message, args) => {
             youtubers_games_text += i + ". " + client.config.games.youtubers_games[key].name + ": `" + key + "`\n";
             i++;
         }
+        i = 1;
+        for (const key in client.config.games.coming_soon) {
+            coming_soon_games_text += i + ". " + client.config.games.coming_soon[key].name + ": `" + key + "`\n";
+            i++;
+        }
         const embed = new Discord.MessageEmbed()
               .setColor('#3333ff')
               .setTitle('Trợ Giúp')
               .addFields(
                 {name: "Game Việt Hoá", value: translated_games_text},
                 {name: "Game Indie", value: indie_games_text},
-                {name: "Dành Cho YouTubers", value: youtubers_games_text}
+                {name: "Dành Cho YouTubers", value: youtubers_games_text},
+                {name: "Sắp ra mắt", value: coming_soon_games_text}
               )
               .setTimestamp()
               .setFooter("Độc quyền của NobiRE VietNam")
         message.channel.send({embeds: [embed]})
         return;
     }
-    var game = client.config.games.translated_games[args.join(" ").toLowerCase()] || client.config.games.indie_games[args.join(" ").toLowerCase()] || client.config.games.youtubers_games[args.join(" ").toLowerCase()];
-    if (!game) return client.sendEmbed('#ff0000', 'Link game', 'Không tìm thấy game này rồi, bạn có nhập nhầm tên game không đấy? :frog:', 'https://i.imgur.com/Ld1y87B.jpeg', 'https://i.imgur.com/Ld1y87B.jpeg')
-    client.sendEmbed('#66ff33', 'Link game', 'Link game nhé: ' + game.link + (game.additional_info ? ("\n" + game.additional_info) : ""), 'https://i.imgur.com/Ld1y87B.jpeg', 'https://i.imgur.com/Ld1y87B.jpeg')
+    var game = client.config.games.translated_games[args.join(" ").toLowerCase()] || client.config.games.indie_games[args.join(" ").toLowerCase()] || client.config.games.youtubers_games[args.join(" ").toLowerCase()] || client.config.games.coming_soon[args.join(" ").toLowerCase()];
+    if (!game) return client.sendEmbed('#ff0000', 'Trợ Giúp', 'Không tìm thấy game này rồi, bạn có nhập nhầm tên game không đấy? :frog:', 'https://i.imgur.com/Ld1y87B.jpeg', 'https://i.imgur.com/Ld1y87B.jpeg')
+    client.sendEmbed('#66ff33', 'Trợ Giúp', 'Link game nhé: ' + game.link + (game.additional_info ? ("\n" + game.additional_info) : ""), 'https://i.imgur.com/Ld1y87B.jpeg', 'https://i.imgur.com/Ld1y87B.jpeg')
 }
