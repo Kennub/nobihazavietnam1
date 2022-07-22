f13af64a3b49049958e96b81b1e04c451baea4de
Kennub <tuantretrau1234@gmail.com>
2022-07-22 17:56:43 +0700

    updated few things

---
+++ commands/info.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.config = {
+    name: "info",
+    description: "Xem thông tin về BOT",
+    accessible_by: "members",
+    dm_supported: false
+}
+
+module.exports.run = (client, message, args) => {
+    var information = "", i = 1;
+        for (const key in client.config.info) {
+            information += i + ". " + client.config.info[key].name + ": `" + client.config.info[key].value + "`\n";
+            i++;
+        }
+    const embed = new Discord.MessageEmbed()
+        .setColor('#3333ff')
+        .setTitle('Trợ Giúp')
+        .addFields(
+            {name: "Thông tin", value: information}
+        )
+        .setTimestamp()
+        .setFooter("Độc quyền bởi NobiRE VietNam")
+        message.channel.send({embeds: [embed]})
+        return;
+}
