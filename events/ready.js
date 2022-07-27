af3efe855afee8bd83fa8ea0761ee42a8502136f
Kennub <tuantretrau1234@gmail.com>
2022-07-27 10:14:28 +0700

    .

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log("Đã đăng nhập với tư cách " + client.user.tag);
-    client.channels.cache.get('909061144948379688').client.sendEmbed("#66ff33", "Welcome", "BOT đã khởi động", "", "");
+    client.channels.cache.get('909061144948379688').sendEmbed("#66ff33", "Welcome", "BOT đã khởi động", "", "");
 };
